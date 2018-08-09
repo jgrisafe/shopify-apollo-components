cc5158475e0fb8f8c2a1babb40a5137c595d3e9e
jgrisafe <jgrisafe@gmail.com>
2018-08-08 22:32:15 -0700

    buildArgsObject progress

--- src/script/utils/buildArgsObject.js
+++ src/script/utils/buildArgsObject.js
@@ -1,44 +1,30 @@
 // @flow
 
-import { getFullType } from './index'
+import { getType, getTypeByName } from './index'
 
-const argumentExtractor = () => {
-  const argsObject = {}
+const argumentExtractor = (query) => {
+  // build the initial argument object from the args array of the base query
+  // this will be built upon with the arguments from the nested fields below
+  const baseArgs = accumulateArgs(query.args)
 
-  return function extract(query, fieldNameArray) {
-    if (!(query && query.args && query.args.length)) {
-      return
+  // loop through the fields and create a new args object for each field that requires
+  // arguments. Most fields will not have any arguments.
+  const baseFieldArgs = getTypeByName(query.type.name).fields.reduce((accum, field) => {
+    if (field.args && field.args.length) {
+      // console.log(getTypeByName(field.type.name)) // eslint-disable-line no-console
+      return Object.assign(accum, { [field.name]: accumulateArgs(field.args) })
     }
+    return accum
+  }, {})
 
-    query.args.forEach((arg) => {
-      if (fieldNameArray && fieldNameArray.length) {
-        return setDeep(argsObject)(fieldNameArray, getFullType(arg.type))
-      }
-      argsObject[arg.name] = getFullType(arg.type)
-    })
-
-    if (query.fields) {
-      query.fields.forEach((field) => {
-        extract(field, [...(fieldNameArray || []), field.name])
-      })
-    }
-
-    return argsObject
-  }
+  return { ...baseArgs, ...baseFieldArgs }
 }
 
-export default argumentExtractor()
 
-function setDeep(obj = {}) {
-  return function set(pathArray, value) {
-    let schema = obj
-    const len = pathArray.length
-    for (let i = 0; i < len - 1; i += 1) {
-      const elem = pathArray[i]
-      if (!schema[elem]) schema[elem] = {}
-      schema = schema[elem]
-    }
+export default argumentExtractor
 
-    schema[pathArray[len - 1]] = value
-  }
+function accumulateArgs(args) {
+  return args.reduce((accum, arg) => (
+    Object.assign(accum, { [arg.name]: getType(arg.type) })),
+  {})
 }
